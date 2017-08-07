5882f45ce5318c1b93420931fb1c863be4faa7b7
Martin Foster <martin.foster@urbanairship.com>
2017-08-07 12:12:56 +0100

    remove simulation checks, add localization, update readme

--- PreferenceCenterSample/PreferenceCenterSample/AppDelegate.swift
+++ PreferenceCenterSample/PreferenceCenterSample/AppDelegate.swift
@@ -7,48 +7,38 @@ import AirshipKit
 import UATagPreferenceCenter
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, UARegistrationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        if TARGET_OS_SIMULATOR == 0 && TARGET_IPHONE_SIMULATOR == 0 {
-            // Set log level for debugging config loading (optional)
-            // It will be set to the value in the loaded config upon takeOff
-            UAirship.setLogLevel(UALogLevel.debug)
-            
-            // Populate AirshipConfig.plist with your app's info from https://go.urbanairship.com
-            // or set runtime properties here.
-            let config = UAConfig.default()
-            
-            // You can then programmatically override the plist values:
-            // config.developmentAppKey = "YourKey"
-            // etc.
-            
-            // Call takeOff (which creates the UAirship singleton)
-            UAirship.takeOff(config)
-            
-            // Print out the application configuration for debugging (optional)
-            print("Config:\n \(config)")
-            
-            // Set the icon badge to zero on startup (optional)
-            UAirship.push()?.resetBadge()
-            
-            // Set self as the UARegistrationDelegate to capture device registration
-            UAirship.push().registrationDelegate = self
-        } else {
-            NSLog("Cannot call takeOff as it looks like you're running in a simulator. Push notications can only be enabled on a device")
-        }
+        // Set log level for debugging config loading (optional)
+        // It will be set to the value in the loaded config upon takeOff
+        UAirship.setLogLevel(UALogLevel.debug)
+        
+        // Populate AirshipConfig.plist with your app's info from https://go.urbanairship.com
+        // or set runtime properties here.
+        let config = UAConfig.default()
+        
+        // You can then programmatically override the plist values:
+        // config.developmentAppKey = "YourKey"
+        // etc.
+        
+        // Call takeOff (which creates the UAirship singleton)
+        UAirship.takeOff(config)
+        
+        // Print out the application configuration for debugging (optional)
+        print("Config:\n \(config)")
+        
+        // Set the icon badge to zero on startup (optional)
+        UAirship.push()?.resetBadge()
+        
+        // Set self as the UARegistrationDelegate to capture device registration
+        UAirship.push().registrationDelegate = self
         
         
         return true
-    }
-    
-    func registrationSucceeded(forChannelID channelID: String, deviceToken: String) {
-        NotificationCenter.default.post(name: MainViewController.registeredChannelID,
-                                        object: self,
-                                        userInfo: nil)
     }
     
 }
